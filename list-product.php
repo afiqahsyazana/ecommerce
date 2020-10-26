af1cf4ff7d95deb42612c4d78efdc0a0150e0d37
afiqahsyazana <56229222+afiqahsyazana@users.noreply.github.com>
2020-10-26 18:54:41 +0800

    Add files via upload

---
+++ list-product.php
@@ -0,0 +1,18 @@
+<html>
+<?php
+//include php header file
+include('header.php');
+?>
+
+<?php
+//include banner area file
+include('template/_special-price.php');
+
+
+?>
+<?php
+// include footer.php file
+include('footer.php');
+?>
+
+</html>
