b197f39e31e54dcbfc49beba21faac84021f1f2a
afiqahsyazana <afiqahstuff@gmail.com>
2020-10-26 18:22:47 +0800

    Initial commit

---
+++ product.php
@@ -0,0 +1,19 @@
+<?php
+//include php header file
+include('header.php');
+?>
+
+<?php
+//include products area file
+include('template/_products.php');
+?>
+
+<?php
+//include banner area file
+include('template/_new-phones.php');
+?>
+
+<?php
+//include php header file
+include('footer.php');
+?>
---
+++ template/_new-phones.php
@@ -0,0 +1,78 @@
+<!-- New Phones -->
+<?php
+$product_shuffle = $product->getData();
+shuffle($product_shuffle);
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (isset($_POST['new_phones_submit'])) {
+        // call method addToCart
+        $Cart->addToCart($_POST['user_id'], $_POST['item_id']);
+    }
+}
+?>
+<section id="new-phones">
+    <div class="container">
+        <h4 class="font-rubik font-size-20">New Phones</h4>
+        <hr>
+
+        <!-- owl carousel -->
+        <div class="owl-carousel owl-theme">
+            <div class="item py-2 bg-light">
+                <div class="product font-rale">
+                    <a href="#"><img src="../proj/assets/products/1.png" alt="product1" class="img-fluid"></a>
+                    <div class="text-center">
+                        <h6>Samsung Galaxy 10</h6>
+                        <div class="rating text-warning font-size-12">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="far fa-star"></i></span>
+                        </div>
+                        <div class="price py-2">
+                            <span>$152</span>
+                        </div>
+                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <?php foreach ($product_shuffle as $item) {
+            ?>
+                <div class="item py-2 bg-light">
+
+                    <div class="product font-rale">
+                        <a href="<?php printf('%s?item_id=%s', 'product.php', $item['item_id']); ?>"><img src="<?php echo $item['item_image'] ?? "../proj/assets/products/1.png"; ?>" alt="product1" class="img-fluid"></a>
+                        <div class="text-center">
+                            <h6><?php echo $item['item_name'] ?? "Unknown"; ?></h6>
+                            <div class="rating text-warning font-size-12">
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="far fa-star"></i></span>
+                            </div>
+                            <div class="price py-2">
+                                <span>RM <?php echo $item["item_price"] ?? 0; ?></span>
+                            </div>
+                            <form method="post">
+                                <input type="hidden" name="item_id" value="<?php echo $item['item_id'] ?? '1'; ?>">
+                                <input type="hidden" name="user_id" value="<?php echo 1; ?>">
+                                <?php
+                                if (in_array($item['item_id'], $Cart->getCartId($product->getData('cart')) ?? [])) {
+                                    echo '<button type="submit" disabled class="btn btn-success font-size-12">In the Cart</button>';
+                                } 
+                                else {
+                                    echo '<button type="submit" name="new_phones_submit" class="btn btn-warning font-size-12">Add to Cart</button>';
+                                }
+                                ?>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+        <!-- !owl carousel -->
+
+    </div>
+</section>
+<!-- !New Phones -->
